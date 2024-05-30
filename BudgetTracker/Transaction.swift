f30f9af9baa466b2906dc5f9701a18fe33b2e331
AkimT13 <akimtarasov@gmail.com>
2024-05-30 15:05:03 -0700

    chnage from struct to class transaction

--- BudgetTracker/Transaction.swift
+++ BudgetTracker/Transaction.swift
@@ -6,7 +6,54 @@
 //
 
 import Foundation
+import SwiftData
 
+
+@Model
+class Transaction: Identifiable {
+    
+    var id: UUID = UUID()
+    var date: Date
+    var institution: String
+    var account: String
+    var merchant: String
+    var amount: Double
+    var type: TransactionType
+    var categoryId: Int
+    var category: String
+    var isPending: Bool
+    var isTransfer: Bool
+    var isExpense: Bool
+    var isEdited: Bool
+    
+    init(date: Date,
+             institution: String,
+             account: String,
+             merchant: String,
+             amount: Double,
+             type: TransactionType,
+             categoryId: Int,
+             category: String,
+             isPending: Bool,
+             isTransfer: Bool,
+             isExpense: Bool,
+             isEdited: Bool) {
+            self.date = date
+            self.institution = institution
+            self.account = account
+            self.merchant = merchant
+            self.amount = amount
+            self.type = type
+            self.categoryId = categoryId
+            self.category = category
+            self.isPending = isPending
+            self.isTransfer = isTransfer
+            self.isExpense = isExpense
+            self.isEdited = isEdited
+    
+    
+}
+/*
 struct Transaction: Codable, Identifiable {
     var id: UUID = UUID()
     var date: Date
@@ -23,7 +70,10 @@ struct Transaction: Codable, Identifiable {
     var isEdited: Bool
 }
 
+ */
 enum TransactionType: String, Codable {
     case credit = "credit"
     case debit = "debit"
 }
+
+
